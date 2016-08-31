d3a11382e28303dc6aad0a21a4178c96b3d6e47c
Karl Isenberg <karl@mesosphere.io>
2016-08-31 11:47:42 -0700

    Optionally interactive

--- govendor.sh
+++ govendor.sh
@@ -25,7 +25,8 @@ if [ -n "${PRIVATE_KEY:-}" ]; then
   PRIVATE_KEY_PARAM="-v ${PRIVATE_KEY:-}:/root/.ssh/id_rsa"
 fi
 
-docker run --rm -i \
+docker run --rm \
+       $([[ "$-" == *i* ]] && echo '-i') \
        $(tty -s && echo '-t') \
        -v "$(pwd):/go/src/${PROJECT_IMPORT}" \
        ${PRIVATE_KEY_PARAM} \
